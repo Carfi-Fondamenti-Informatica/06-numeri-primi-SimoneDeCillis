7659d6177299cce2cd65f29919458a91d37e6a06
SimoneDeCillis <116493653+SimoneDeCillis@users.noreply.github.com>
2022-12-01 13:05:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+using namespace std;
+#include "lib.h"
+
 
 int main() {
     int n ;
@@ -11,5 +14,3 @@ int main() {
     {
         cout<<"numero non primo"<<endl;
     }
- return 0;
-}
