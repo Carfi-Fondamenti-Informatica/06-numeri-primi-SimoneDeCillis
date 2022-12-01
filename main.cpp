c6cf1f9cfbcef7273a0858925a5ed85c34d65b80
SimoneDeCillis <116493653+SimoneDeCillis@users.noreply.github.com>
2022-12-01 12:45:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
-int main(){
-  return 0;
+int main() {
+    int n ;
+    cin>> n;
+    bool risultato= funzione(n);
+    if (risultato == true){
+        cout<<"numero primo"<<endl;
+    }
+    else
+    {
+        cout<<"numero non primo"<<endl;
+    }
+
+
+    return 0;
 }
